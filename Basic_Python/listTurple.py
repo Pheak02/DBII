8e0678c485d014e7fe329ca8a46a184bb77bbf24
Pheak02 <saingsopheak02@gmail.com>
2023-10-18 09:28:09 +0700

    list & turple

---
+++ Basic_Python/listTurple.py
@@ -0,0 +1,11 @@
+list =["pheak", "mony", "many"]
+print(list[0])
+turple =("pheak", "mony", "many")
+print(tuple[0])
+#list of number
+numbers = [1,2,3,4,5]
+list=[]
+print(numbers*2)
+for i in numbers:
+    list.append(i*2)
+print(list)
